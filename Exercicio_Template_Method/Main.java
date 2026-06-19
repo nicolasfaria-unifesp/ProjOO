8aaaa77c74c53f5531a1f5937be5aea39b31d7d4
Nicolas Almeida Faria <nicolas.faria@unifesp.br>
2026-06-19 14:28:11 -0300

    Implementando Main

---
+++ Exercicio_Template_Method/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        List<String> palavras = new ArrayList<>();
+        palavras.add("Java");
+        palavras.add("Python");
+        palavras.add("C++");
+        palavras.add("Ama");
+        palavras.add("Brasil");
+
+        palavras.sort(new ComparadorUltimaLetra());
+
+        for(String palavra : palavras) {
+            System.out.println(palavra);
+        }
+    }
+}
